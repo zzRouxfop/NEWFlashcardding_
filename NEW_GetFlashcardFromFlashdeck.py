8741cd5c28308338c74db7e48705ae17d8c5d8d2
zzRxoufop <shuffleitupyourass@gmail.com>
2024-12-08 18:46:39 -0500

    Update NEW_GetFlashcardFromFlashdeck.py

--- NEW_GetFlashcardFromFlashdeck.py
+++ NEW_GetFlashcardFromFlashdeck.py
@@ -3,19 +3,22 @@ from CommaManagement import *
 
 def GetFlashcardFromFlashdeck():
     printAllFlashdecks()
-    Title = str(input("which flashdeck would you like to view the flashcards from?: "))
-    fTitle = 'flashdecks/' + Title + '.txt'
-    try:
-        with open(fTitle, 'r') as f:
-            Flashdeck = []
-            for l in f:
-                Flashdeck.append(khmerconverter(l))
-
-            counter = 1
-            for l in Flashdeck:
-                print(f'\n{counter}:\n{l[0]}\n{l[1]}')
-                counter += 1
-            print('\n')
-
-    except FileNotFoundError:
-        print(f'{Title} not found!')
+    Title = str(input("which flashdeck would you like to view the flashcards from? [press ENTER to cancel]: "))
+    if Title == '':
+        pass
+    else:
+        fTitle = 'flashdecks/' + Title + '.txt'
+        try:
+            with open(fTitle, 'r') as f:
+                Flashdeck = []
+                for l in f:
+                    Flashdeck.append(khmerconverter(l))
+    
+                counter = 1
+                for l in Flashdeck:
+                    print(f'\n{counter}:\n{l[0]}\n{l[1]}')
+                    counter += 1
+                print('\n')
+    
+        except FileNotFoundError:
+            print(f'{Title} not found!')
