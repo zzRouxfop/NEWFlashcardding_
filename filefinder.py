fadeb8c0f8e14e7a4ba48a4b20aa03dcc0638a2f
zzRxoufop <shuffleitupyourass@gmail.com>
2024-12-08 01:58:16 -0500

    Update filefinder.py

--- filefinder.py
+++ filefinder.py
@@ -1,10 +1,11 @@
 import os
+import time
 
-def printAllFlashdecks(directory = 'flashdecks/'):
+def printAllFlashdecks(directory = 'flashdecks'):
     if directory not in os.listdir():
-        makeDir = input(f"there is no flashdecks directory called '{directory}'. Make one? [Y/n]")
-        if makeDir.lower() == 'y':
-            os.mkdir('flashdecks')
+        makeDir = input(f"there is no flashdecks directory called '{directory}'. One will be made shortly...")
+        sleep(5)
+        os.mkdir('flashdecks')
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         print(filename[0:-4])
