3512f9c528cf96c8ae4caf52a64aeb0a20d19bb4
Dylan Sommer <dylansommer96@gmail.com>
2024-12-07 21:31:39 -0500

    added error-handling for absence of flashdecks directory when looking for flashdecks.

--- filefinder.py
+++ filefinder.py
@@ -1,6 +1,10 @@
 import os
 
 def printAllFlashdecks(directory = 'flashdecks/'):
+    if directory not in os.listdir():
+        makeDir = input(f"there is no flashdecks directory called '{directory}'. Make one? [Y/n]")
+        if makeDir.lower() == 'y':
+            os.mkdir('flashdecks')
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         print(filename)
