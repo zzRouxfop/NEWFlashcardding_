dc66c6f65972e95f50dde9285f1945a584e39e9f
zzRxoufop <shuffleitupyourass@gmail.com>
2024-12-08 23:46:23 -0500

    AAAAA

--- NEW_Interface.py
+++ NEW_Interface.py
@@ -8,11 +8,11 @@ from DelFlashcard import *
 
 def MainMenu():
     while True:
-        print('- - - - - - - - - - \ntype [L] to get flashdecks\ntype [M] to make flashdeck\ntype [P] to get flashcards of a flashdeck\n- - - - - - - - - - \ntype [F] to practice a flashdeck\ntype [Q] to quiz yourself on a flashdeck\n- - - - - - - - - -\ntype [E] to edit an existing flashdeck\ntype [D] to delete a flashdeck\ntype [X] to exit\n- - - - - - - - - - \n')
+        print('- - - - - - - - - - \ntype [L] to get flashdecks\ntype [M] to make flashdeck\ntype [P] to get flashcards of a flashdeck\n- - - - - - - - - - \ntype [F] to practice a flashdeck\ntype [Q] to quiz yourself on a flashdeck\n- - - - - - - - - -\ntype [E] to edit an existing flashdeck\ntype [D] to delete a flashdeck\ntype [X] to exit\n- - - - - - - - - - ')
         inputting = str(input(">>> "))
 
         inputting = inputting.strip().lower()
-
+        print('\n')
         if inputting == 'm':
             makeFlashdeck()
         elif inputting == 'l':
